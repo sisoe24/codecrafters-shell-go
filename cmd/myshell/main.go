beef9412a1f118a1e8afa59365dcf7ae621c9c22
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2024-12-14 13:35:27 -0500

    The echo builtin

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,10 +4,22 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
+
+type Command struct {
+  command string
+  args []string
+}
+
+
+func parseInput(input string) Command {
+  args := strings.Split(input, " ")
+  return Command{command: args[0], args: args[1:]}
+}
 
 func main() {
   fmt.Fprint(os.Stdout, "$ ")
@@ -16,13 +28,16 @@ func main() {
 
 	for scanner.Scan() {
 
-    command := scanner.Text()
+    input := scanner.Text()
+    command := parseInput(input)
 
-		switch command {
-    case "exit 0":
+		switch  command.command {
+    case "echo":
+			fmt.Printf("%s\n", strings.Join(command.args, " "))
+    case "exit":
       os.Exit(0)
 		default:
-			fmt.Printf("%s: command not found\n", command)
+			fmt.Printf("%s: command not found\n", input)
 		}
 
     fmt.Fprint(os.Stdout, "$ ")
